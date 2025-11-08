3c3d47b3f1c0ff20dbeb33a8559e8608c1e688bf
pasindusgkmp <pasindugunasekara889@gmail.com>
2025-11-08 12:21:29 +0530

    http request postman check done

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,16 +1,34 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "api/v/user")
+@RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
     @GetMapping("/getUser")
     public String getUser() {
         return "User data";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser() {
+        return "Save user";
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser() {
+        return "Update user";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser() {
+        return "Delete user";
     }
 }
